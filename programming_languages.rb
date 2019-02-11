e125ce2ec93f0b4faa197f87d1bd2aafe8866efb
Kevin Ruggiero <kevinbruggiero@gmail.com>
2019-02-11 16:00:54 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,13 @@
+
 def reformat_languages(languages)
-  # your code here
+  new_languages = {}
+  languages.each { |style, language|
+    language.each { |name, value|
+      new_languages[name] = value 
+      new_languages[name][:style] = []
+      new_languages[name][:style] << style
+    }
+  }
+  new_languages[:javascript][:style] << :oo
+  new_languages
 end
